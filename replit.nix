fa0261dd721bd6cbb8ed0f58f53d0593a4ac83f8
johnatanvallejomarulanda <johnatanvallejomarulanda@gmail.com>
2026-01-12 04:49:47 +0000

    ok

--- replit.nix
+++ replit.nix
@@ -2,11 +2,11 @@
     deps = [
       pkgs.nodejs_20,
       pkgs.postgresql,
-      pkgs.openssl_1_1,
+      pkgs.openssl_3,
       pkgs.pkg-config
     ];
     env = {
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl_1_1 ];
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl_3 ];
       PRISMA_CLI_QUERY_ENGINE_TYPE = "binary";
     };
 }
