362e90eaae4a204368bba80cc00e8efd43065aec
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 02:46:28 +0000

    ok

--- start_db.sh
+++ start_db.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/ephemeral/nix/store/03h8f1wmpb86s9v8xd0lcb7jnp7nwm6l-idx-env-fhs/usr/bin/pg_ctl -D pgdata -l logfile start
+# Initialize the database if it hasn't been already
+if [ ! -d "pgdata" ]; then
+  initdb pgdata
+fi
+
+# Start the PostgreSQL server
+pg_ctl -D pgdata -l logfile start
