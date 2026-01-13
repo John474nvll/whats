97390a21a51320341800e41825e3f6150dbd8707
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 08:24:31 +0000

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -106,7 +106,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:5001',
         changeOrigin: true,
       },
     },
