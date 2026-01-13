baab3d47c9858a142437d481f5e61f688f3ee842
john474nvallejo <john474nvallejo@gmail.com>
2026-01-13 07:37:33 +0000

    ok

--- start_db.sh
+++ start_db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Initialize the database if it hasn't been already
-if [ ! -d "pgdata" ]; then
-  initdb pgdata
+set -e
+PG_CTL=/nix/store/j6y1jsy8f16ps655vf1i1z788mv11q83-postgresql-16.3/bin/pg_ctl
+INITDB=/nix/store/j6y1jsy8f16ps655vf1i1z788mv11q83-postgresql-16.3/bin/initdb
+PGDATA=/home/user/whats/pgdata
+LOGFILE=/home/user/whats/logfile
+if [ ! -d "$PGDATA" ]; then
+  echo "Initializing database..."
+  $INITDB -D "$PGDATA"
 fi
-
-# Start the PostgreSQL server
-pg_ctl -D pgdata -l logfile start
+echo "Starting PostgreSQL and waiting for it to be ready..."
+$PG_CTL -D "$PGDATA" -l "$LOGFILE" -w start
