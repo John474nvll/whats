e094860cc02da31d4abf1603caf754b17da67e2d
john474nvallejo <john474nvallejo@gmail.com>
2026-01-18 20:37:45 +0000

    ok

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,16 @@
-
 # This file defines the development environment for your project.
 # It's used by your IDE to install the necessary packages.
 { pkgs }: {
     packages = [
         # Node.js and npm
         pkgs.nodejs-20_x
-        pkgs.nodePackages.npm
+        pkgs.nodePackages.npm,
 
-        # PostgreSQL database
-        pkgs.postgresql
-        pkgs.terminator
+        # SQLite database
+        pkgs.sqlite,
 
         # Tools for running scripts from package.json
-        pkgs.nodePackages.tsx
+        pkgs.nodePackages.tsx,
         pkgs.drizzle-kit
     ];
-    startup = {
-      init = {
-        command = "mkdir -p /run/postgresql && chown -R $USER:$USER /run/postgresql";
-        once = true;
-      };
-    };
 }
