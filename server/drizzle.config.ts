9d19f2080ad2affcb0e43b3498ab0a5104f7179a
john474nvallejo <john474nvallejo@gmail.com>
2026-01-19 01:06:15 +0000

    ok

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,8 +2,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema/*",
-  out: "./src/db/migrations",
+  schema: "./db/schema/*",
+  out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
