7b997bdd551f7ff2c101e48fc17e4027027b7689
john474nvallejo <john474nvallejo@gmail.com>
2026-01-25 18:52:37 +0000

    ok

--- replit.nix
+++ replit.nix
@@ -1,8 +1,7 @@
 nix
-{
-  pkgs ? import <nixpkgs> {}
-}:
-
+let
+  pkgs = import <nixpkgs> {};
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     openssl
@@ -11,16 +10,7 @@ pkgs.mkShell {
   ] ++ (with pkgs.python3Packages; [
     requests
     pycryptodome
-    (buildPythonPackage {
-      pname = "curl_cffi";
-      version = "0.6.2"; # Specify version 0.6.2 as a baseline for >=0.6.2
-      src = pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-R47vFz2w3m3j4j2j5k1k0l1k2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f"; # Placeholder, actual hash needed for specific version
-      };
-      # If curl_cffi depends on other Nix packages, list them here:
-      # buildInputs = [ pkgs.libcurl ];
-    })
+    curl_cffi
     aiohttp
     certifi
     browser_cookie3
@@ -39,6 +29,10 @@ pkgs.mkShell {
     python-dotenv
     ddgs
     cloudscraper
-    pip
   ]);
+
+  # Set environment variables for curl_cffi and other potential needs
+  shellHook = ''
+    export PYTHONNOUSERSITE=1
+  '';
 }
