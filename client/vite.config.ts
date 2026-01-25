48e94bb339549b0ac59b511e6a44191aba640add
john474nvallejo <john474nvallejo@gmail.com>
2026-01-25 20:02:18 +0000

    ok

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:9002',
         changeOrigin: true,
         secure: false,
       },
