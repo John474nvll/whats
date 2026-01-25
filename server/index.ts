7ff5bebe54d5f8841c0614c4dd4a2e4e9e9d6af4
john474nvallejo <john474nvallejo@gmail.com>
2026-01-25 05:03:35 +0000

    ok

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
+import { program } from "commander";
 
 const app = express();
 const httpServer = createServer(app);
@@ -60,6 +61,14 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+
+  program
+    .option("-p, --port <port>", "Port to run the server on", "5000")
+    .option("-h, --host <host>", "Host to run the server on", "0.0.0.0")
+    .parse(process.argv);
+
+  const options = program.opts();
+
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
@@ -84,11 +93,12 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(options.port, 10);
+  const host = options.host;
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
+      host,
       reusePort: true,
     },
     () => {
