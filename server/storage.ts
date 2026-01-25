3fe7cd98dded12005cb961895f9a4c32ca7776d0
john474nvallejo <john474nvallejo@gmail.com>
2026-01-25 05:15:51 +0000

    ok

--- server/storage.ts
+++ server/storage.ts
@@ -1,12 +1,13 @@
 
 import { db } from "./db";
 import {
-  users, contacts, conversations, messages,
+  users, contacts, conversations, messages, customers,
   type InsertUser, type User,
   type InsertContact, type Contact,
   type InsertConversation, type Conversation,
   type InsertMessage, type Message,
-  type ConversationWithContact
+  type ConversationWithContact,
+  type Customer, type InsertCustomer
 } from "@shared/schema";
 import { eq, desc } from "drizzle-orm";
 import { InMemoryStorage } from "./storage-mem";
@@ -18,8 +19,11 @@ export interface IStorage {
   createUser(user: InsertUser): Promise<User>;
 
   // Contacts
+  getContacts(): Promise<Contact[]>;
   getContactByPlatformId(platform: string, platformId: string): Promise<Contact | undefined>;
   createContact(contact: InsertContact): Promise<Contact>;
+  updateContact(id: number, contact: Partial<InsertContact>): Promise<Contact | undefined>;
+  deleteContact(id: number): Promise<void>;
 
   // Conversations
   getConversations(): Promise<ConversationWithContact[]>;
@@ -32,6 +36,12 @@ export interface IStorage {
   getMessages(conversationId: number): Promise<Message[]>;
   createMessage(message: InsertMessage): Promise<Message>;
   getLastMessage(conversationId: number): Promise<Message | undefined>;
+  
+  // Customers
+  getCustomers(): Promise<Customer[]>;
+  createCustomer(customer: InsertCustomer): Promise<Customer>;
+  updateCustomer(id: number, customer: Partial<InsertCustomer>): Promise<Customer | undefined>;
+  deleteCustomer(id: number): Promise<void>;
 }
 
 export const storage = new InMemoryStorage();
