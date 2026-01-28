a6a93fd5c19faef572b3a471a7e66367e26c3b7e
john474nvallejo <john474nvallejo@gmail.com>
2026-01-28 08:06:51 +0000

    ok

---
+++ server/services/auth.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  // Placeholder for token verification logic
+  next();
+};
