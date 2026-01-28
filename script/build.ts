4b70ec9a490d19034e01d501ee75dbaae5adb9e4
john474nvallejo <john474nvallejo@gmail.com>
2026-01-28 06:15:28 +0000

    ok

--- script/build.ts
+++ script/build.ts
@@ -24,7 +24,7 @@ async function buildApp() {
     // Build server
     await new Promise((resolve, reject) => {
       const esbuildProcess = exec(
-        'esbuild server/index.ts --bundle --platform=node --outfile=dist/index.cjs --format=cjs',
+        'esbuild server/index.ts --bundle --platform=node --outfile=dist/index.cjs --format=cjs --external:@babel/preset-typescript --external:@vite-pwa/assets-generator',
         { cwd: root },
         (error, stdout, stderr) => {
           if (error) {
