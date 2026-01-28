b626a79bfccc3aef43d93f71aa7f495c98123e62
john474nvallejo <john474nvallejo@gmail.com>
2026-01-28 07:34:58 +0000

    ok

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,10 @@ export default defineConfig({
       includeAssets: ['favicon.png', 'robots.txt', 'apple-touch-icon.png', 'offline.html'],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'), // Example for assets
+    },
+  },
 });
