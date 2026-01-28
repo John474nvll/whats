9da71468162ab8076621054668c5a5cba9ffb935
john474nvallejo <john474nvallejo@gmail.com>
2026-01-28 07:28:34 +0000

    ok

---
+++ client/vite.config.ts
@@ -0,0 +1,14 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'robots.txt', 'apple-touch-icon.png', 'offline.html'],
+    }),
+  ],
+});
